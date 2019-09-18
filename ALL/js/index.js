3bc74f5c673857ca52f87abff4a7fb73dd71773c
Joe <535137168@qq.com>
2019-09-18 21:27:48 +0800

    111

---
+++ ALL/js/index.js
@@ -0,0 +1,23 @@
+
+
+
+$(function () {
+    var $bj=$('header')
+
+    // $bj.css('background','url(../header-bg1.jpg)');
+    var num=1;
+
+    // setInterval(function () {
+    //     num++;
+    //     if (num>4){
+    //
+    //         num=1
+    //     }
+    //     console.log(num)
+    // },1000)
+
+
+})
+
+
+
