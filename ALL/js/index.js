42af2fcd97dfec39194f9ce349e41a939664c98b
Joe <535137168@qq.com>
2019-09-20 17:39:23 +0800

    111

--- ALL/js/index.js
+++ ALL/js/index.js
@@ -1,23 +1,76 @@
 
-
-
 $(function () {
-    var $bj=$('header')
 
-    // $bj.css('background','url(../header-bg1.jpg)');
-    var num=1;
+    // 回到顶部
+    var $a=$('.shouye');
+    $a.click(function () {
+        $("html,body").animate({scrollTop:0},500)
+    })
 
-    // setInterval(function () {
-    //     num++;
-    //     if (num>4){
-    //
-    //         num=1
-    //     }
-    //     console.log(num)
-    // },1000)
+    //导航固定
+
+        var banOffTop=$(".nav").offset().top;//获取到距离顶部的垂直距离
+        var scTop=0;//初始化垂直滚动的距离
+    var secs = document.querySelectorAll('.content section');
+    var $lis=$('.nav ul li');
+    // var lis=document.querySelectorAll('.nav ul li a')[1];.offsetTop
+        $(document).scroll(function(){
+            scTop=$(this).scrollTop();
+            if(scTop>=banOffTop){
+                $(".nav").addClass("show");
+            }else{
+                $(".nav").removeClass("show");
+            }
+
+        })
+
+
+
+
+
+
+
+
+//    音乐暂停
+        var $aud = $('.music')[0]
+        var $stop= $('.stop')
+        var num=0;
+        $stop.click(function () {
+            num++;
+            if (num%2==0){
+                $aud.play();
+            }else {
+                $aud.pause();
+            }
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
-
-
 
